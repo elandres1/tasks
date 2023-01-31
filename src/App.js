28a6db205c079933d05d3eccd63e7fd773049882
AndresHelloWorld <andres.angulob@pca.edu.co>
2023-01-31 18:36:16 -0500

    s

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ function App() {
     <div className="App">
       <form onSubmit={(e)=>handleSubmit(e)}>
         <input type="text"
+        
           placeholder='Enter a new task'
           onChange={(e) => setTask(e.target.value)}
         />
